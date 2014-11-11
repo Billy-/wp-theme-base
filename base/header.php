06715512d89a921ae0d34ba7c0bd4ea0c368bba0
Billy <billy@studiopretty.com>
2014-11-11 01:09:16 +0000

    Main template files

---
+++ base/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <title><?php wp_title(); ?></title>
+                <meta name="viewport" value="width=device-width, initial-scale=1">
+                <?php wp_head(); ?>
+        </head>
+        <body>
