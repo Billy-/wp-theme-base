17bc1f562b3bfd7bb2ffab38058aa0c481bd89f5
Billy <billy@studiopretty.com>
2014-11-20 11:58:32 +0000

    Added Modernizr and html classes (no-js, lt-ie*)

--- base/functions.php
+++ base/functions.php
@@ -1,7 +1,9 @@
 <?php 
 function enqueue_my_scripts() {
-	/* jQuery dependency is assumed */
-	wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ));
-	wp_enqueue_style('theme-css', get_stylesheet_directory_uri() . '/css/styles.css');
+    /* Uncompressed development version. Download custom build for production. */
+    wp_enqueue_script('Modernizr', get_stylesheet_directory_uri() . '/js/modernizr.js');
+    /* jQuery dependency is assumed */
+    wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ));
+    wp_enqueue_style('theme-css', get_stylesheet_directory_uri() . '/css/styles.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_my_scripts' );
