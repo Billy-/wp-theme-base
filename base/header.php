1c418ba9eceb23806455ca16c89069f8b90efbec
Billy <billy@studiopretty.com>
2014-11-20 12:50:41 +0000

    Added <meta charset="utf-8"> into <head> and fixed some indentations

--- base/header.php
+++ base/header.php
@@ -3,9 +3,10 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-        <head>
-                <title><?php wp_title(); ?></title>
-                <meta name="viewport" value="width=device-width, initial-scale=1">
-                <?php wp_head(); ?>
-        </head>
-        <body>
+    <head>
+        <meta charset="utf-8">
+        <title><?php wp_title(); ?></title>
+        <meta name="viewport" value="width=device-width, initial-scale=1">
+        <?php wp_head(); ?>
+    </head>
+    <body>
