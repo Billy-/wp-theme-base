4f0f9f8d44eea4902892a018a25ab5917de92766
Billy <billy@studiopretty.com>
2014-11-25 14:07:52 +0000

    Removal of $_POST and other general cleaning and fixed a couple of typos

--- base/page-contact.php
+++ base/page-contact.php
@@ -5,23 +5,22 @@ $email_invalid		= '<p class="error notification">Please enter a valid email addr
 $message_invalid	= '<p class="error notification">Please enter a message.</p>';
 $message_unsent		= '<p class="error notification">Sorry, your message was not sent. Please try Again.</p>';
 $message_sent		= '<p class="success notification">Thanks! Your message has been sent.</p>';
-$submitted	= $_POST['form_submitted'];
-$name		= $_POST['message_name'];
-$email		= filter_var( $_POST['message_email'], FILTER_VALIDATE_EMAIL );
-$message	= $_POST['message_text'];
+$submitted	= filter_input( INPUT_POST, 'form_submitted', FILTER_VALIDATE_BOOLEAN );
+$name		= filter_input( INPUT_POST, 'message_name', FILTER_SANITIZE_STRING );
+$from_email	= filter_input( INPUT_POST, 'message_email', FILTER_VALIDATE_EMAIL );
+$message	= filter_input( INPUT_POST, 'message_text', FILTER_SANITIZE_STRING );
 $responses	= Array();
 if ( !!$submitted ){
-    if ( empty( $name ) ) $responses[] = $fname_invalid;
-    if ( $email === false ) $responses[] = $email_invalid;
+    if ( empty( $name ) ) $responses[] = $name_invalid;
+    if ( empty( $from_email ) ) $responses[] = $email_invalid;
     if ( empty( $message ) ) $responses[] = $message_invalid;
 
     if ( empty( $responses ) ) {
-        $body = 'Message from: ' . $fname . $lname . '\r\n';
-        $body .= strip_tags($message);
-        $to 		= get_option('admin_email');
-        $subject 	= "Someone sent a message from ".get_bloginfo('name');
-        $headers 	= 'From: '. $email_filtered . "\r\n" . 'Reply-To: ' . $email_filtered . "\r\n";
-        $sent = wp_mail($to, $subject, $body, $headers);
+        $headers 	= 'From: '. $from_email . "\r\n" . 'Reply-To: ' . $from_email . "\r\n";
+        $subject 	= "Someone sent a message from " . get_bloginfo('name');
+        $body           = 'Message from: ' . $name . '\r\n' . $message;
+        $to_email	= get_option('admin_email');
+        $sent = wp_mail($to_email, $subject, $body, $headers);
         if ($sent){
                 $responses[] = $message_sent;
         } else {
@@ -43,12 +42,12 @@ get_template_part('partials/banner-top');
         <?php forEach( $responses as $response ){
             echo $response;
         } ?>
-        <input placeholder="Name" type="text" class="text-box" name="message_name" value="<?php echo esc_attr($_POST['message_name']); ?>"></label>
-        <input placeholder="Email" type="text" class="text-box" name="message_email" value="<?php echo esc_attr($_POST['message_email']); ?>"></label>
-        <textarea placeholder="Message"type="text" class="text-box" name="message_text" rows="6"><?php echo esc_textarea($_POST['message_text']); ?></textarea></label>
+        <input placeholder="Name" type="text" class="text-box" name="message_name" value="<?php echo $name; ?>"></label>
+        <input placeholder="Email" type="text" class="text-box" name="message_email" value="<?php echo $from_email; ?>"></label>
+        <textarea placeholder="Message"type="text" class="text-box" name="message_text" rows="6"><?php echo $message; ?></textarea></label>
         <input type="hidden" name="form_submitted" value="1">
         <input type="submit" class="button">
     </form>
 </article>
 
-<?php get_footer(); ?>
+<?php get_footer();
