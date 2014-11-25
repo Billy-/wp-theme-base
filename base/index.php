25d677d95cade7cc10f70ebd5b633148c83ccf32
Billy <billy@studiopretty.com>
2014-11-25 14:21:14 +0000

    Minor formatting changes

--- base/index.php
+++ base/index.php
@@ -1,18 +1,14 @@
-<?php
-get_header();
-?>
+<?php get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<article>
-		<header>
-			<a href="<?php the_permalink(); ?>">
-				<h2><?php the_title(); ?></h2>
-			</a>
-		</header>
-		<?php the_content(); ?>
-	</article>
+    <article>
+        <header>
+            <a href="<?php the_permalink(); ?>">
+                <h2><?php the_title(); ?></h2>
+            </a>
+        </header>
+        <?php the_content(); ?>
+    </article>
 <?php endwhile; endif; ?>
 
-<?php
-get_footer();
-?>
+<?php get_footer();
