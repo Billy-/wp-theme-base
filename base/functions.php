96527d8e6f9d786b8dd5af266b15607434992030
Billy <billy@studiopretty.com>
2015-03-04 03:22:17 +0000

    Added main navigation menu registration

--- base/functions.php
+++ base/functions.php
@@ -8,3 +8,8 @@ function enqueue_my_scripts() {
     wp_enqueue_style('theme-css', get_stylesheet_directory_uri() . '/css/styles.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_my_scripts' );
+
+function register_menu() {
+  register_nav_menu( 'main_nav', 'Main Navigation Menu' );
+}
+add_action( 'after_setup_theme', 'register_menu' );
