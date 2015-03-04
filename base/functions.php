cae7425e07abcd8d91e2f63a82031a491a05c253
Billy <billy@studiopretty.com>
2015-03-04 03:17:32 +0000

    Added CSS reset

--- base/functions.php
+++ base/functions.php
@@ -4,6 +4,7 @@ function enqueue_my_scripts() {
     wp_enqueue_script('Modernizr', get_stylesheet_directory_uri() . '/js/modernizr.js');
     /* jQuery dependency is assumed */
     wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/js/scripts.js', array( 'jquery' ));
+    wp_enqueue_style('css-reset', get_stylesheet_directory_uri() . '/css/reset.css');
     wp_enqueue_style('theme-css', get_stylesheet_directory_uri() . '/css/styles.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_my_scripts' );
